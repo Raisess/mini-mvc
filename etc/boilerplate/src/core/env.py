9fe7a817a341b7d8e1b3a3bc86e4703ec22b8563
Raisess <raisechif@gmail.com>
2023-08-02 09:12:00 -0300

    feat: add `.env` support

---
+++ etc/boilerplate/src/core/env.py
@@ -0,0 +1,17 @@
+import os
+
+class Env:
+  __DATA = {}
+
+  @staticmethod
+  def Init() -> None:
+    with open(".env", "r") as file:
+      lines = file.read().strip().split("\n")
+      for line in lines:
+        cols = line.strip().split("=")
+        if len(cols) == 2:
+          Env.__DATA[cols[0]] = cols[1].replace("\"", "").replace("'", "")
+
+  @staticmethod
+  def Get(key: str) -> str | None:
+    return Env.__DATA.get(key) or os.getenv(key)
