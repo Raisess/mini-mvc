b324c029d4310edb50fb2390706210cb0fba0558
Raisess <raisechif@gmail.com>
2023-10-27 10:36:15 -0300

    fix: update `update` script

--- etc/boilerplate/scripts/update.sh
+++ etc/boilerplate/scripts/update.sh
@@ -5,6 +5,6 @@ CURRENT_DIR=$(echo $PWD)
 python3 -m ensurepip
 python3 -m pip install -r ./requirements.txt
 
-rm -r $CURRENT_DIR/src/core
+rm -r $CURRENT_DIR/src/__core
 cd /tmp && rm -rf ./mini-mvc && git clone https://github.com/Raisess/mini-mvc
-cp -r ./mini-mvc/etc/boilerplate/src/core $CURRENT_DIR/src
+cp -r ./mini-mvc/etc/boilerplate/src/__core $CURRENT_DIR/src
