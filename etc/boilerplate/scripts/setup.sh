67b6e0812b72c1132c780dea11423d6b8db3577e
Raisess <raisechif@gmail.com>
2024-09-27 14:21:52 -0300

    chore: improve boilerplate scripts

--- etc/boilerplate/scripts/setup.sh
+++ etc/boilerplate/scripts/setup.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 
+echo "Mini MVC setup is running..."
+
 rm -rf ./.git
 
 python3 -m ensurepip
 python3 -m pip install -r ./requirements.txt
 
 git init && git branch -m main
+
+echo "Done!"
+
+bash ./scripts/update.sh
--- etc/boilerplate/scripts/update.sh
+++ etc/boilerplate/scripts/update.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
-CURRENT_DIR=$(echo $PWD)
+echo "Updating Mini MVC core..."
 
-python3 -m ensurepip
-python3 -m pip install -r ./requirements.txt
+CURRENT_DIR=$(echo $PWD)
 
 rm -r $CURRENT_DIR/src/__core
 cd /tmp && rm -rf ./mini-mvc && git clone https://github.com/Raisess/mini-mvc
 cp -r ./mini-mvc/etc/boilerplate/src/__core $CURRENT_DIR/src
+
+echo "Done!"
