5be894ffb81521b47c2ad668e56707a4fdab7987
Raisess <raisechif@gmail.com>
2024-09-28 10:50:32 -0300

    hotfix: sql databases `__init__`

--- etc/boilerplate/src/__core/plugins/database/sql/__init__.py
+++ etc/boilerplate/src/__core/plugins/database/sql/__init__.py
@@ -1,2 +1,2 @@
-from __core.plugins.database.postgresql import PostgreSQL
-from __core.plugins.database.sqlite import SQLite
+from __core.plugins.database.sql.postgresql import PostgreSQL
+from __core.plugins.database.sql.sqlite import SQLite
